253cec4c547c8f6891232a3cee9a4e41639c2c1f
wcm950305 <863401948@qq.com>
2018-12-26 11:04:57 +0800

    day24 第四次作业

--- day19-设计模式/src/org/westos/demo1/TestFlyweight.java
+++
@@ -1,9 +0,0 @@
-package org.westos.demo1;
-
-public class TestFlyweight {
-    public static void main(String[] args) {
-        System.out.println(Integer.valueOf(12)==Integer.valueOf(12));
-        System.out.println(Integer.valueOf(127)==Integer.valueOf(127));
-        System.out.println(Integer.valueOf(129)==Integer.valueOf(129));
-    }
-}
